e15b8f341252a226a47a3a8f0995d73679d999eb
Zofux <70540447+Zofux@users.noreply.github.com>
2024-10-16 13:25:48 +0200

    Update

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 window.onload = function () {
+    // Kode for funksjonaliteten til infoboxsene 
+
     const htmlBox = document.getElementById("html")
     const cssBox = document.getElementById("css")
     const jsBox = document.getElementById("js")
@@ -7,14 +9,19 @@ window.onload = function () {
     const chevron2 = document.getElementById("chevron2")
     const chevron3 = document.getElementById("chevron3")
 
+    // HTML boksen
     htmlBox.addEventListener("click", async () => {
         htmlBox.classList.toggle("open")
         chevron1.classList.toggle("open")
     })
+
+    // CSS boksen
     cssBox.addEventListener("click", async () => {
         cssBox.classList.toggle("open")
         chevron2.classList.toggle("open")
     })
+
+    // Javascript boksen
     jsBox.addEventListener("click", async () => {
         jsBox.classList.toggle("open")
         chevron3.classList.toggle("open")
